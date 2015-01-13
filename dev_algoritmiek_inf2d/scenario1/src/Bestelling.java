f43ed266d11f1a6d8065c5dbb0d1afbbdf7cf47d
Hoye <hoyelam96@gmail.com>
2015-01-13 10:17:18 +0100

    bestelling class

--- dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
+++ dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
@@ -1,5 +1,17 @@
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by Fabian on 13-1-2015.
  */
+
+
 public class Bestelling {
+    int klantID;
+    int bestellingID;
+    boolean verwerking;
+    Date tijd = new Date();
+    String duur;
+    boolean compleet;
+    boolean dadelijk;
 }
