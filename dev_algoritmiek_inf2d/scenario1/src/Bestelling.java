a52538e5306aa052b1343c14e9ea98e2ddee272d
Hoye <hoyelam96@gmail.com>
2015-01-13 11:10:31 +0100

    voer klant gegevens in

--- dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
+++ dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
@@ -1,17 +1,26 @@
 import java.sql.Time;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Fabian on 13-1-2015.
  */
 
 
-public class Bestelling {
-    int klantID;
-    int bestellingID;
-    boolean verwerking;
-    Date tijd = new Date();
-    String duur;
-    boolean compleet;
-    boolean dadelijk;
+public class Bestelling extends Klant{
+
+    public void plaatsBestelling(){
+        int bestellingID;
+        boolean verwerking;
+        Date tijd = new Date();
+        String duur;
+        boolean compleet;
+        boolean dadelijk;
+
+
+    }
+
+
+
 }
