15cef3dbc36538bf1c6f3ea36343403e1d8839bf
Hoye <hoyelam96@gmail.com>
2015-01-20 11:05:52 +0100

    scenario 1 af

--- dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
+++ dev_algoritmiek_inf2d/scenario1/src/Bestelling.java
@@ -1,4 +1,5 @@
 import java.sql.Time;
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.*;
@@ -18,11 +19,16 @@ public class Bestelling extends Klant{
     static int klantid = randomGenerator.nextInt(1000);
     static int bestellingid = randomGenerator.nextInt(1000);
 
-    static String voornaam = "hoye1";
-    static String achternaam = "lam";
-    static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    static boolean Verwerking;
+    static String Duur = "1 minuut";
+    static boolean Compleet;
+    static boolean Dadelijk;
 
-    public static void main(String args[]) {
+    static String compleet_tijd = "";
+    static String start_tijd = "";
+
+    public static void main(String args[])
+    {
         Bestelling.kiesOptie();
     }
 
@@ -53,12 +59,48 @@ public class Bestelling extends Klant{
     public static void voegKlant(){
         Klant test = Klant.getStreamInstance();
         Calendar cal = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
 
-        Timer timer = new Timer();
+        Scanner in = new Scanner(System.in);
 
-        cal.getTime();
+        System.out.println("Voer uw achternaam in ");
+        achternaam = in.nextLine();
+        System.out.println("Uw achternaam is " + achternaam);
 
-        test.addKlant("Meneer/Mevrouw: "+ voornaam + " " + achternaam + " met klant id: " + klantid + "bestelling id: "+ bestellingid + "starttijd: "+ sdf + "duur is: 1 minuut");
+        System.out.println("Voer uw voornaam in ");
+        voornaam = in.nextLine();
+        System.out.println("Uw voornaam is " + voornaam);
+
+        System.out.println("Voer uw leeftijd in ");
+        leeftijd = in.nextInt();
+        System.out.println("Uw leeftijd is " + leeftijd);
+
+        System.out.println("Voer uw geslacht in M of V");
+        geslacht = in.nextLine();
+        System.out.println("Uw geslacht is " + geslacht);
+
+        System.out.println("Voer uw woonplaats in ");
+        plaats = in.nextLine();
+        System.out.println("Uw achternaam is " + plaats);
+
+        System.out.println("Voer uw e-mailadress in ");
+        email = in.nextLine();
+        System.out.println("Uw email is " + email);
+
+        cal.add(Calendar.MINUTE, queue.size());
+        cal2.add(Calendar.MINUTE, queue.size()+1);
+
+        DateFormat sdf = new SimpleDateFormat("HH:mm");
+        compleet_tijd = sdf.format(cal2.getTime());
+        start_tijd = sdf.format(cal.getTime());
+
+        Verwerking = true;
+        Dadelijk = true;
+        Compleet = false;
+
+        test.addKlant(voornaam + " " + achternaam + " Klant id: " + klantid + " bestelling id: " + bestellingid + " klaar rond: "
+                + compleet_tijd + " start tijd: "+ start_tijd+ " Verwerking: " + Verwerking + " Dadelijk: " + Dadelijk + " Compleet :"
+                + Compleet + " Bestelling klaar in: " + Duur + "Na starttijd");
         System.out.println("Queue is momenteel:" + test.get());
 
         Bestelling.kiesOptie();
@@ -67,9 +109,28 @@ public class Bestelling extends Klant{
 
     public static void updateQueue(){
         Klant test = Klant.getStreamInstance();
-        System.out.println("De volgende klant is klaar: " + test.poll());
-        System.out.println("Queue is momenteel:" + test.get());
+        Calendar cal = Calendar.getInstance();
 
+        DateFormat sdf = new SimpleDateFormat("HH:mm");
+        String current_tijd = sdf.format(cal.getTime());
+
+        Integer i = 0;
+
+        for (i = 0; i < queue.size(); i++){
+            if (current_tijd != compleet_tijd)
+            {
+                Verwerking = false;
+                Dadelijk = false;
+                Compleet = true;
+                System.out.println("De volgende klant is klaar: " + Compleet + test.poll());
+                System.out.println("Queue is momenteel:" + test.get());
+            }
+            else
+            {
+                System.out.println("Queue is momenteel:" + test.get());
+            }
         }
+        Bestelling.kiesOptie();
+    }
 }
 
